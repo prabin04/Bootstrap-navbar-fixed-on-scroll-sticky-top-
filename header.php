eb348c97d38a7b410e424a37216d295ed82fc89d
Prabin Parajuli <prabin.b9@gmail.com>
2020-07-28 10:22:33 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+<div class="bg-warning py-2">
+	<div class="container">
+		Some top header info
+	</div>
+</div>
+<nav id="navbar_top" class="navbar navbar-expand-lg navbar-dark bg-primary">
+<div class="container">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="main_nav">	
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+      <li class="nav-item"><a class="nav-link" href="#"> Menu item </a></li>
+    </ul>
+  </div> <!-- navbar-collapse.// -->
+</div> <!-- container.// -->
+</nav>
+</header>
